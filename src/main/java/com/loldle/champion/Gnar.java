56e641fd460a4945d78b70dd729dbde1a2c145ba
Wezabi <Westonhuhman@gmail.com>
2022-11-05 12:03:18 -0500

    Add the forgotten G champions

---
+++ src/main/java/com/loldle/champion/Gnar.java
@@ -0,0 +1,76 @@
+package com.loldle.champion;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+
+import java.util.List;
+
+import com.loldle.Champion;
+import com.loldle.type.Gender;
+import com.loldle.type.RangeType;
+import com.loldle.type.Region;
+import com.loldle.type.Resource;
+import com.loldle.type.Role;
+import com.loldle.type.Species;
+
+/**
+ * <p>
+ * Implementation of Gnar.
+ * </p>
+ * <p>
+ * Copyright &copy; 2022 
+ * </p>
+ *
+ * @Author Wes Huhman
+ */
+public class Gnar implements Champion
+{
+
+	@Override
+	public String getName()
+	{
+		return "Gnar";
+	}
+
+	@Override
+	public Gender getGender()
+	{
+		return Gender.MALE;
+	}
+
+	@Override
+	public List<Role> getRoles()
+	{
+		return singletonList(Role.TOP);
+	}
+
+	@Override
+	public List<Species> getSpecies()
+	{
+		return singletonList(Species.YORDLE);
+	}
+
+	@Override
+	public Resource getResource()
+	{
+		return Resource.RAGE;
+	}
+
+	@Override
+	public List<RangeType> getRangeTypes()
+	{
+		return asList(RangeType.MELEE, RangeType.RANGED);
+	}
+
+	@Override
+	public List<Region> getRegions()
+	{
+		return singletonList(Region.FRELJORD);
+	}
+
+	@Override
+	public int getReleaseYear()
+	{
+		return 2014;
+	}
+}
